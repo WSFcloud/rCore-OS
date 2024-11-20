eb7d6da2e0fefbd7f4a3b07c2e7c358aca3a2c9a
WSFcloud <byhtdcc@gmail.com>
2024-11-20 13:28:21 +0800

    Apply changes from ch2 to ch3

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -2,6 +2,7 @@
 
 use crate::sbi::shutdown;
 use core::panic::PanicInfo;
+use crate::stack_trace::print_stack_trace;
 use log::*;
 
 #[panic_handler]
@@ -16,5 +17,6 @@ fn panic(info: &PanicInfo) -> ! {
     } else {
         error!("[kernel] Panicked: {}", info.message().unwrap());
     }
+    unsafe { print_stack_trace(); }
     shutdown(true)
 }
--- os/src/main.rs
+++ os/src/main.rs
@@ -37,6 +37,7 @@ pub mod syscall;
 pub mod task;
 mod timer;
 pub mod trap;
+mod stack_trace;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
